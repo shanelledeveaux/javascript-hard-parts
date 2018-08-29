6248af941ea49f860346733a55e7f5e4c9335fc8
Shanelle Webster <shanellewebster@Shanelles-MacBook-Pro.local>
2018-08-29 13:48:52 -0500

    principals of javascript

---
+++ willsentance.js
@@ -0,0 +1,42 @@
+//WHAT HAPPENS WHEN JAVASCRIPT RUNS MY CODE?
+
+const num = 3;
+function multiplyBy2(inputNumber) {
+  const result = inputNumber * 2;
+  return result;
+}
+
+const name = "Will";
+
+// processes code line by line. 'Just in time compilation'.
+// assigns data to labels.'
+
+//GLOBAL MEMORY
+//num = 3;
+//multiplyBy2 : ASSIGNED TO FUNCITON
+// name = 'Will';
+
+//FUNCTION IS NOT RUN BECAUSE IT IS NOT CALLED. NOTHING INSIDE THE FUNCTION IS ASSIGNED UNTIL THE FUNCTION IS CALLED.
+
+// When we start running the code, we create a global execution context.
+// -- Thread of execution (parsing and executing the code line by line)
+// -- Live memory of variables with data (known as Global Variable Environment)
+
+//THE THREAD
+// -- JavaScript can only do one thing at a time. Called SINGLE THREAD.
+// -- Sychronous execution.
+
+const num = 3;
+function multiplyBy2(inputNumber) {
+  const result = inputNumber * 2;
+  return result;
+}
+
+const output = multiplyBy2(4);
+const newOutput = multiplyBy2(10);
+
+// RUNNING/ CALLING/ INVOKING A FUNCTION
+// -- not the same as defining a function.
+// -- create a new execution context compromising:
+// ---- the thread of execution (goes through the code in the function line by line)
+// ---- a local memory (Variable environment) where anything defined in the function is stored.
